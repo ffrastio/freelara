e65a2755e6470681d6cb8572f049e3ab289155b2
Fani Frastio <fanitl2a@gmail.com>
2022-04-04 11:20:05 +0700

    route setting

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,11 @@
 <?php
 
+use App\Http\Controllers\Dashboard\MemberController;
+use App\Http\Controllers\Dashboard\MyOrderController;
+use App\Http\Controllers\Dashboard\ProfileController;
+use App\Http\Controllers\Dashboard\RequestController;
+use App\Http\Controllers\Dashboard\ServiceController;
+use App\Http\Controllers\Landing\LandingController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,10 +19,43 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('explore', LandingController::class, 'explore')->name('explore.landing');
+Route::get('detail_booking/{id}', LandingController::class, 'detail_booking')->name('detail.booking.landing');
+Route::get('booking/{id}', LandingController::class, 'booking')->name('booking.landing');
+Route::get('detail/{id}', LandingController::class, 'detail')->name('detail.landing');
+Route::resource('/', LandingController::class);
 
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
+// Route Dashboard Authentication
+
+Route::group(
+    ['prefix' => 'member', 'as' => 'member.', 'middleware' => ['auth:sanctum', 'verified']],
+    function () {
+
+        // Route Dashboard
+        Route::resource('dashboard', MemberController::class);
+
+        // Route service
+        Route::resource('service', ServiceController::class);
+
+        // myOrder
+        Route::get('accept/order/{id}', MyOrderController::class, 'accepted')->name('accept.order');
+        Route::get('reject/order/{id}', MyOrderController::class, 'rejected')->name('reject.order');
+        Route::resource('order', MyOrderController::class);
+
+        // Request
+        Route::get('approve/request/{id}', RequestController::class, 'approve')->name('approve.request');
+        Route::resource('request', RequestController::class);
+
+        // Profile
+        Route::get('delete_photo', ProfileController::class, 'delete')->name('delete.photo.profile');
+        Route::resource('profile', ProfileController::class);
+    }
+);
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+//     return view('dashboard');
+// })->name('dashboard');
